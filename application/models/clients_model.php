00e9a1c95fcb5f22849e362864e9794ddae1b5a3
Shadow2004 <chrispratt1985@gmail.com>
2014-01-21 22:14:17 +0000

    Missed client model changes.

--- application/models/clients_model.php
+++ application/models/clients_model.php
@@ -1,31 +1,1 @@
-<?php
-
-class Users_model extends CI_Model {
-
-    public function __construct() {
-        $this->load->database();
-    }
-
-    public function get_users($userID = FALSE) {
-        if ($userID === FALSE) {
-            $query = $this->db->get('users');
-            return $query->result_array();
-        }
-
-        $query = $this->db->get_where('users', array('id' => $userID));
-        return $query->row_array();
-    }
-
-    public function set_users() {
-        $data = array(
-            'firstName' => $this->input->post('fname'),
-            'lastName' => $this->input->post('lname'),
-            'address' => $this->input->post('address'),
-            'dateOfBirth' => $this->input->post('dateofbirth'),
-            'phoneNumber' => $this->input->post('phone'),
-            'email' => $this->input->post('email')
-        );
-
-        return $this->db->insert('users', $data);
-    }
-}
+<?phpclass Clients_Model extends CI_Model {    public function __construct() {        $this->load->database();    }    public function get_clients($userID = FALSE) {        if ($userID === FALSE) {            $query = $this->db->get('users');            return $query->result_array();        }        $query = $this->db->get_where('users', array('id' => $userID));                return $query->row_array();    }    public function set_clients($data) {        return $this->db->insert('users', $data);    }}
